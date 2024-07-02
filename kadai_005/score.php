c23d802b3ea6c0c38f7bffa9d024e1e5aa83f958
akakimu <162883466+akakimu@users.noreply.github.com>
2024-07-02 23:09:27 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,13 @@
+<!DOCTPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP課題_テストの平均点を計算しよう</title>
+    </head>
+
+    <body>
+        <?php
+        echo (80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60) / 10;
+        ?>
+        </body>
+</html>
