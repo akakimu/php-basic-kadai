21f51cadc73f9013791a38a9233b4f06415b23fc
akakimu <162883466+akakimu@users.noreply.github.com>
2024-07-02 23:38:03 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -7,7 +7,22 @@
 
     <body>
         <?php
-        echo (80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60) / 10;
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        $score_total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+        $score_average = $score_total / 10;
+        echo $score_average;
+
         ?>
         </body>
 </html>
