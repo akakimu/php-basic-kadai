a4dc5df50f3f84fc6067817b8c7c756a0ef5d627
akakimu <162883466+akakimu@users.noreply.github.com>
2024-07-06 16:14:35 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP課題_連想配列を作って値とキーを出力しよう</title>
+    </head>
+
+    <body>
+        <p>
+            <?php
+            $personal_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+            print_r($personal_data);
+            ?>
+        </p>
+    </body>
+</html>
